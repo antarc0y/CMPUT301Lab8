66e572bfe69cd3e9f0fe92caa90c8a262de9fc7e
yuihan <juyi@ualberta.ca>
2022-10-30 17:21:13 -0600

    add to custom list test case

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import android.content.Context;
+import android.view.View;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CustomListTest {
-
-
+    public CustomList mockCity() {
+        CustomList list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+    @Test
+    public void testAdd() {
+        CustomList mockList = mockCity();
+        int size = mockList.getCount();
+        mockList.addCity(new City("Edmonton","Alberta"));
+        assertEquals(mockList.getCount(),size+1);
+    }
 }
